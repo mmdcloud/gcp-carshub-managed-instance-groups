0986b639dac21dc2023e66925d85333bba07d0fa
mmdcloud <madmaxcloudpractice@gmail.com>
2025-05-25 13:03:30 +0530

    update: removed unnecessary files

--- backend/api/artifact_push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir backend-code
-cp -r ../backend/api/* backend-code/
-cd backend-code
-
-docker buildx build --tag carshub-backend --file ./Dockerfile .
-docker tag carshub-backend:latest us-central1-docker.pkg.dev/orbital-bee-455915-h5/carshub-backend/carshub-backend:latest
-docker push us-central1-docker.pkg.dev/orbital-bee-455915-h5/carshub-backend/carshub-backend:latest
